c178bd4108eef4522adf8603a57dc4b7250dc4ad
ITS-Cyber <115570947+ITS-Cyber@users.noreply.github.com>
2022-10-12 17:51:14 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
-pkg install python3
-pkg install git
-apt install git
-pkg install wget
-pkg install curl
-pkg install python -y
-pkg install python2 -y
-pkg install python3 -y
-apt install curl -y
-apt install php
-pkg install ruby
-gem install lolcat
-apt install figlet
-pkg install fish -y
-apt update
-apt upgrade
+mkdir $HOME/.termux/ ;echo "extra-keys = [['ESC','/','-','HOME','UP','END'],['TAB','CTRL','ALT','LEFT','DOWN','RIGHT']]" >> $HOME/.termux/termux.properties && termux-reload-settings
+
+pkg install -y python
+pkg install -y python2
+pkg install -y python3
+apt install -y git
+pkg install -y wget
+apt install -y curl
+apt install -y php
+pkg install -y ruby
+gem install  lolcat
+apt install  figlet
+pkg install -y fish
+pkg install -y w3m
+apt update -y
+apt upgrade -y
+
+
+
+figlet done | lolcat
+
+exit
